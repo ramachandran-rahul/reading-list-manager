c20e9545a9ce33dae81aa64065f7dcbdf3f83ee6
ramachandran-rahul <ramachandran.k.rahul@gmail.com>
2024-09-12 23:42:02 +1000

    Initial Commit

---
+++ ReadingListManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReadingListManager
+//
+//  Created by Rahul Ramachandran on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReadingListManager/ReadingListManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingListManagerApp.swift
+//  ReadingListManager
+//
+//  Created by Rahul Ramachandran on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadingListManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
